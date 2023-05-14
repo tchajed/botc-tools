dd0f3ac7018340858d6ead277379660e88dabb5a
Tej Chajed <tchajed@gmail.com>
2023-05-14 11:01:54 -0700

    Move one helper to views

--- src/roles_view.ts
+++ src/roles_view.ts
@@ -1,27 +1,13 @@
 import { CharacterInfo } from './botc/roles';
 import { Script } from './botc/script';
 
-import { iconPath } from './views';
+import { characterIconElement, htmlToElements } from './views';
 
 import h from 'hyperscript';
 import hh from 'hyperscript-helpers';
-const { div, h1, img, strong, table, tbody, tr, td } = hh(h);
+const { div, h1, strong, table, tbody, tr, td } = hh(h);
 
 import classnames from 'classnames';
-
-function htmlToElement(html: string): HTMLElement {
-  var template = document.createElement('template');
-  html = html.trimStart(); // Avoid creating a whitespace node
-  template.innerHTML = html;
-  return template.content.firstChild as HTMLElement;
-}
-
-function htmlToElements(html: string): ChildNode[] {
-  var template = document.createElement('template');
-  html = html.trimStart(); // Avoid creating a whitespace node
-  template.innerHTML = html;
-  return Array.from(template.content.childNodes);
-}
 
 function createHeaderHTML(title: string): HTMLElement {
   return h1(div(title));
@@ -34,13 +20,8 @@ function abilityHTML(ability: string): ChildNode[] {
 function createCharacterHTML(character: CharacterInfo): HTMLElement {
   var cells = [];
 
-  cells.push(td(".icon-cell",
-    iconPath(character.id) ? div(".img-container",
-      img(".char-icon", { src: iconPath(character.id) })
-    ) : []))
-
+  cells.push(td(".icon-cell", characterIconElement(character)));
   cells.push(td(".name-cell", [character.name]));
-
   cells.push(td(".ability-cell", abilityHTML(character.ability)));
 
   return tr({ className: classnames(character.evil ? "evil" : "good") }, cells);
