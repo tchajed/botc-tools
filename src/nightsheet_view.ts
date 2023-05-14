9195ab6c7ac5a8cfc2518d4b9505e9da8422e69b
Tej Chajed <tchajed@gmail.com>
2023-05-14 11:23:44 -0700

    Migrate nightsheet_view to hyperscript

--- src/nightsheet_view.ts
+++ src/nightsheet_view.ts
@@ -1,4 +1,12 @@
+import { CharacterInfo } from './botc/roles';
 import { NightOrders, Script } from './botc/script';
+
+import { characterIconElement, htmlToElements, iconPath } from './views';
+import classnames from 'classnames';
+
+import h from 'hyperscript';
+import hh from 'hyperscript-helpers';
+const { div, h1, a, table, tbody, tr, td } = hh(h);
 
 const tokenNames = new Set([
   "THIS IS THE DEMON",
@@ -10,69 +18,71 @@ const tokenNames = new Set([
 ]);
 
 function createHeader(title: string, firstNight: boolean): HTMLElement {
-  const element = document.createElement("h1");
-  element.insertAdjacentHTML("beforeend", `<div>${title}</div>`);
   const nightLabel = firstNight ? "FIRST NIGHT" : "OTHER NIGHTS";
-  element.insertAdjacentHTML("beforeend", `<div class="label">${nightLabel}</div>`);
-  return element;
+  return h1(
+    [
+      div(title),
+      div(".label", nightLabel),
+    ]
+  );
+}
+
+function detailsElement(details: string | null): ChildNode[] {
+  if (!details) {
+    return [];
+  }
+  var details = details;
+  details = details.replace(/If [^.]*:/g, '\n$&\n');
+  details = details.trim();
+  details = details.replace(/\n/g, "<br/>");
+  details = details.replace(/\<tab\>/g, "&nbsp;&nbsp;&nbsp;&nbsp;");
+  for (const tokenName of tokenNames) {
+    details = details.replace(tokenName, `<strong>${tokenName}</strong>`);
+    var altTokenName = tokenName.charAt(0).toUpperCase() + tokenName.substring(1).toLowerCase();
+    altTokenName = `'${altTokenName}'`;
+    details = details.replace(altTokenName, `<strong>${tokenName}</strong>`);
+  }
+  return htmlToElements(details);
+}
+
+function characterRow(character: CharacterInfo, firstNight: boolean): HTMLElement {
+  var cells = [];
+
+  cells.push(td(".icon-cell", characterIconElement(character)));
+  var nameCell = [character.name];
+  if (iconPath(character.id)) {
+    nameCell = a({ href: `https://wiki.bloodontheclocktower.com/${character.name}` }, [nameCell]);
+  }
+  cells.push(td(".name-cell", nameCell));
+  const details = detailsElement(character.nightDetails(firstNight).details);
+  cells.push(td({
+    className: classnames(
+      "details", "details-cell",
+      details.length == 0 ? "empty" : null)
+  }, details));
+
+  return tr({ className: classnames(character.evil ? "evil" : "good") }, cells);
 }
 
 function createCharacterList(orders: NightOrders, firstNight: boolean): HTMLElement {
-  var charList = document.createElement("table");
-  var charHTML = "";
-  charHTML += "<tbody>";
+  var rows = [];
   for (const character of firstNight ? orders.firstNight : orders.otherNights) {
-    const align = character.evil ? "evil" : "good";
     if (character.nightDetails(firstNight)) {
-      charHTML += `<tr class="${align}">`;
-
-      charHTML += `<td class="icon-cell">`
-      if (iconPath(character.id)) {
-        charHTML += `<div class="img-container"><img class="char-icon" src=${iconPath(character.id)}></div>`;
-      }
-      charHTML += `</td>`
-      charHTML += `<td class="name-cell">`;
-      // heuristic for whether this is a real character
-      if (iconPath(character.id)) {
-        charHTML += `<a href="https://wiki.bloodontheclocktower.com/${character.name}">
-          ${character.name}
-        </a>`;
-      } else {
-        charHTML += `${character.name}`;
-      }
-      charHTML += `</td>`;
-      var details = character.nightDetails(firstNight).details || "";
-      details = details.replace(/If [^.]*:/g, '\n$&\n');
-      details = details.trim();
-      details = details.replace(/\n/g, "<br/>");
-      details = details.replace(/\<tab\>/g, "&nbsp;&nbsp;&nbsp;&nbsp;");
-      for (const tokenName of tokenNames) {
-        details = details.replace(tokenName, `<strong>${tokenName}</strong>`);
-        var altTokenName = tokenName.charAt(0).toUpperCase() + tokenName.substring(1).toLowerCase();
-        altTokenName = `'${altTokenName}'`;
-        details = details.replace(altTokenName, `<strong>${tokenName}</strong>`);
-      }
-      charHTML += `<td class="details details-cell ${details == "" ? "empty" : ""}">${details}</td>`;
-      charHTML += `</tr>`;
+      rows.push(characterRow(character, firstNight));
     }
   }
-  charHTML += "</tbody>";
-  charList?.insertAdjacentHTML("beforeend", charHTML);
-  return charList;
+  return table(tbody(rows));
 }
 
 function createJinxesElement(script: Script): HTMLElement {
-  const div = document.createElement("div");
-  div.classList.add("jinxes")
-  div.classList.add("details")
+  var els = [];
   for (const jinx of script.jinxes) {
     for (const char of [jinx.character1, jinx.character2]) {
-      div.insertAdjacentHTML("beforeend",
-        `<div class="img-container"><img class="char-icon" src="${iconPath(char)}"></div>`);
+      els.push(...characterIconElement({ id: char }));
     }
-    div.insertAdjacentHTML("beforeend", `<div class="jinx">${jinx.description}</div>`);
+    els.push(div(".jinx", [jinx.description]));
   }
-  return div;
+  return div(".jinxes .details", els);
 }
 
 function createSheetElement(script: Script, firstNight: boolean): HTMLElement {
@@ -92,8 +102,7 @@ export function loadScriptToDOM(script: Script) {
 }
 
 // import script from '../assets/scripts/laissez_un_carnaval.json';
-import script from '../assets/scripts/faith_trust_and_pixie_dust.json';
-import { iconPath } from './views';
-// import script from '../assets/scripts/visitors.json';
+// import script from '../assets/scripts/faith_trust_and_pixie_dust.json';
+import script from '../assets/scripts/visitors.json';
 // import script from '../assets/scripts/sects_and_violets.json';
 loadScriptToDOM(new Script(script));
