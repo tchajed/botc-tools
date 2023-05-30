9969f550f1954db30967856681f2c5fe81823969
Tej Chajed <tchajed@gmail.com>
2023-05-29 22:06:43 -0700

    Show random shuffle of selection at bottom
    This is the core functionality! Now we just implement some bells and
    whistles around automatic feedback for characters that modify setup like
    Drunk and Baron.

--- src/js/randomizer_view.ts
+++ src/js/randomizer_view.ts
@@ -5,7 +5,7 @@ import { characterIconElement, htmlToElements } from './views';
 
 import h from 'hyperscript';
 import hh from 'hyperscript-helpers';
-const { div, h1, strong, br, span, label, input } = hh(h);
+const { div, h1, strong, br, span, label, input, hr } = hh(h);
 
 import classnames from 'classnames';
 import { selectedScript } from './select_script';
@@ -17,19 +17,39 @@ function createHeaderHTML(title: string): HTMLElement {
 
 var state = new Randomizer();
 
-function distributionForCount(numPlayers: number): { townsfolk: number, outsiders: number, minions: number, demons: number } {
-  const demons = 1;
-  var outsiders = null;
-  var minions = null;
+interface Distribution {
+  townsfolk: number,
+  outsider: number,
+  minion: number,
+  demon: number,
+}
+
+function distributionForCount(numPlayers: number): Distribution {
+  const demon = 1;
+  var outsider = null;
+  var minion = null;
   if (numPlayers == 5 || numPlayers == 6) {
-    outsiders = numPlayers - 5;
-    minions = 1;
+    outsider = numPlayers - 5;
+    minion = 1;
   } else {
-    outsiders = (numPlayers - 7) % 3;
-    minions = Math.floor((numPlayers - 7) / 3) + 1;
+    outsider = (numPlayers - 7) % 3;
+    minion = Math.floor((numPlayers - 7) / 3) + 1;
   }
-  const townsfolk = numPlayers - outsiders - minions - demons;
-  return { townsfolk, outsiders, minions, demons };
+  const townsfolk = numPlayers - outsider - minion - demon;
+  return { townsfolk, outsider, minion, demon };
+}
+
+// TODO: better naming
+function distributionHTML(dist: Distribution): HTMLElement {
+  return span(
+    [span(".good", dist.townsfolk.toString()),
+      " / ",
+    span(".good", dist.outsider.toString()),
+      " / ",
+    span(".evil", dist.minion.toString()),
+      " / ",
+    span(".evil", dist.demon.toString()),
+    ]);
 }
 
 function createDistributionHTML(state: Randomizer): HTMLElement {
@@ -37,15 +57,7 @@ function createDistributionHTML(state: Randomizer): HTMLElement {
     return span("");
   }
   let dist = distributionForCount(state.getNumPlayers());
-  return span(
-    [span(".good", dist.townsfolk.toString()),
-      " / ",
-    span(".good", dist.outsiders.toString()),
-      " / ",
-    span(".evil", dist.minions.toString()),
-      " / ",
-    span(".evil", dist.demons.toString()),
-    ]);
+  return distributionHTML(dist);
 }
 
 function createPlayerSelectHTML(): HTMLElement {
@@ -74,27 +86,30 @@ function createPlayerSelectHTML(): HTMLElement {
   ]);
 }
 
-function toggleCharacter(id: string, e: Event) {
-  var sel = state.toggleSelected(id);
-  if (e.currentTarget instanceof Element) {
-    const classes = e.currentTarget.classList;
-    if (sel) {
-      classes.add("selected");
-    } else {
-      classes.remove("selected");
+function toggleCharacter(id: string): (Event) => void {
+  return (e) => {
+    var sel = state.toggleSelected(id);
+    if (e.currentTarget instanceof Element) {
+      const classes = e.currentTarget.classList;
+      if (sel) {
+        classes.add("selected");
+      } else {
+        classes.remove("selected");
+      }
     }
+    updateSelected();
   }
 }
 
-function createCharacterHTML(character: CharacterInfo): HTMLElement {
+function createCharacterHTML(character: CharacterInfo, interactive: boolean): HTMLElement {
   var roleLabel = null;
   if (["outsider", "minion"].includes(character.roleType)) {
     roleLabel = span(".role-label", character.roleType.charAt(0).toUpperCase());
   }
-  state.addCharacter(character);
+  var onclick = interactive ? toggleCharacter(character.id) : null;
   return div({
     className: classnames(character.evil ? "evil" : "good", "character"),
-    onclick: (e: Event) => toggleCharacter(character.id, e),
+    onclick,
   },
     [
       roleLabel,
@@ -103,36 +118,49 @@ function createCharacterHTML(character: CharacterInfo): HTMLElement {
     ])
 }
 
-function createCharactersList(characters: CharacterInfo[]): HTMLElement {
-  var rows = [];
-  for (const character of characters) {
-    rows.push(createCharacterHTML(character));
-  }
-  var numColumns = 3;
-  const numPerColumn = Math.ceil(rows.length / numColumns);
-  var taken = 0;
+function createCharacterColumns(characters: CharacterInfo[], numColumns: number): CharacterInfo[][] {
+  const numPerColumn = Math.ceil(characters.length / numColumns);
   var columns = [];
-  while (rows.length > 0) {
-    var num = numPerColumn;
-    // this isn't the last column
-    if (rows.length > num &&
-      // and the last two of this row have different roles
-      characters[taken + num - 1].roleType !=
-      characters[taken + num - 2].roleType) {
-      // ... so move the last one to the next column
-      num--;
-    }
-    // if the next column would have a single role at the top, take it earlier
-    if (rows.length > 1 &&
-      taken + num + 1 < characters.length &&
-      characters[taken + num].roleType != characters[taken + num + 1].roleType) {
-      num++;
-    }
-    let col = rows.splice(0, num);
+  while (characters.length > 0) {
+    let col = characters.splice(0, numPerColumn);
     columns.push(col);
-    taken += col.length;
   }
-  return div(".characters", columns.map(col => div(".column", col)));
+  return columns;
+}
+
+function createCharactersList(characters: CharacterInfo[]): HTMLElement {
+  for (const character of characters) {
+    state.addCharacter(character);
+  }
+  var cols = createCharacterColumns(characters, 3);
+  return div(".characters", cols.map(col =>
+    div(".column", col.map(character =>
+      createCharacterHTML(character, true)
+    )),
+  ));
+}
+
+function createSelectedCharactersHTML(): HTMLElement {
+  var selected = state.allSelected();
+  selected.sort((i1, i2) => Math.random() - Math.random());
+  var distribution: Distribution = { townsfolk: 0, outsider: 0, minion: 0, demon: 0 };
+  for (const char of selected) {
+    distribution[char.roleType]++;
+  }
+  const cols = createCharacterColumns(selected, 4);
+  return div([
+    distributionHTML(distribution),
+    div(".selected-characters", cols.map(col =>
+      div(".column", col.map(char =>
+        createCharacterHTML(char, false)
+      ))))
+  ]);
+}
+
+function updateSelected() {
+  const selected = document.getElementById("selected");
+  selected.innerHTML = "";
+  selected.insertAdjacentElement("beforeend", createSelectedCharactersHTML());
 }
 
 function createApp(script: Script): HTMLElement[] {
@@ -140,6 +168,8 @@ function createApp(script: Script): HTMLElement[] {
     createHeaderHTML(script.title),
     createPlayerSelectHTML(),
     createCharactersList(script.characters),
+    hr(".separator"),
+    div("#selected"),
   ]
 }
 
