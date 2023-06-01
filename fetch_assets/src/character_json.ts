bf976250de472c62dccb48dd7330187daac199c6
Tej Chajed <tchajed@gmail.com>
2023-05-31 22:06:38 -0700

    Add accidentally deleted JSON download

--- fetch_assets/src/character_json.ts
+++ fetch_assets/src/character_json.ts
@@ -4,11 +4,15 @@ import fs from 'fs';
 /** Download JSON files with character metadata and text. */
 export async function downloadCharacterData(assetsPath: string) {
   // there's also tether.json and game-characters-restrictions.json which we don't download
-  const downloads = ["roles", "jinx", "nightsheet"].map(file => {
+  var downloads = ["roles", "jinx", "nightsheet"].map(file => {
     return {
       url: `https://script.bloodontheclocktower.com/data/${file}.json`,
       name: `${file}.json`,
     };
+  });
+  downloads.push({
+    url: "https://raw.githubusercontent.com/bra1n/townsquare/develop/src/roles.json",
+    name: "botc_online_roles.json",
   });
   await Promise.all(downloads.map(async (file) => {
     let destFile = `${assetsPath}/${file.name}`;
