20316dc7c43d9c6f100d0a089e09cc7ca8c76be0
Tej Chajed <tchajed@gmail.com>
2023-06-01 21:41:05 -0700

    Configure workbox correctly

--- workbox-config.js
+++ workbox-config.js
@@ -3,6 +3,13 @@ module.exports = {
   globPatterns: [
     "**/*.{html,js,css,png,svg,jpg,gif,json,woff,woff2,eot,ico,webmanifest,map}"
   ],
+  runtimeCaching: [{
+    urlPattern: new RegExp('https://fonts.(?:googleapis|gstatic).com/(.*)'),
+    handler: 'CacheFirst',
+    options: {
+      cacheName: 'google-fonts',
+    },
+  }],
   swDest: "dist/service-worker.js",
   clientsClaim: true,
   skipWaiting: true
