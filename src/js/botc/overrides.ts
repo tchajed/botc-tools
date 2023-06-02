bcd31b3a393bee85f67f8dd13458623ca6077665
Tej Chajed <tchajed@gmail.com>
2023-06-02 14:30:45 -0700

    Better documentation and organization of overrides

---
+++ src/js/botc/overrides.ts
@@ -0,0 +1,104 @@
+// To show nothing for a night reminder, set it to an empty string "".
+//
+// nights sets both firstNight and otherNights implicitly
+
+export interface Override {
+  ability?: string;
+  nights?: string;
+  firstNight?: string;
+  otherNights?: string;
+}
+
+const overrideList: { [key: string]: Override } = {
+  "philosopher": {
+    nights: "",
+  },
+  "investigator": {
+    firstNight: "",
+  },
+  "chef": {
+    firstNight: "Show the number of pairs of neighboring evil players.",
+  },
+  "clockmaker": {
+    firstNight: "Show the number of places from Demon to closest Minion.",
+  },
+  "lunatic": {
+    firstNight: `If 7 or more players: <tab>Show THESE ARE YOUR MINIONS. Point to "Minions".
+    <tab>Show three bluffs.
+    <tab>Put the Lunatic to sleep. Wake the demon.
+    <tab>Show the YOU ARE token, and the Demon token.
+    <tab>Show THIS PLAYER IS and the Lunatic token, point to the Lunatic.
+    `,
+    otherNights: "Do whatever is needed to simulate the demon.",
+  },
+  "cerenovus": {
+    nights: `The Cerenovus chooses a player and character. Wake the target.
+    Show THIS CHARACTER SELECTED YOU, the Cerenovus token, and the madness character.`,
+  },
+  "sailor": {
+    nights: "",
+  },
+  "poisoner": {
+    nights: "The Poisoner picks a player to poison.",
+  },
+  "fortuneteller": {
+    nights: "The Fortune Teller points to two players. Give a yes if one is the Demon (or red herring)."
+  },
+  "cultleader": {
+    nights: "The Cult Leader might change alignment to match an alive neighbor. If it changed, tell the Cult Leader their new alignment.",
+  },
+  "monk": {
+    nights: "The Monk protects a player from the Demon.",
+  },
+  "innkeeper": {
+    nights: "The Innkeeper points to two players, who are both safe from the Demon. One is drunk.",
+  },
+  "imp": {
+    otherNights: `The Imp kills a player. If they chose themselves,
+    replace an alive Minion with an Imp token. Show them YOU ARE and then the Imp token.`,
+  },
+  "towncrier": {
+    otherNights: "Give a 'yes' or 'no' for if a Minion nominated today.",
+  },
+
+  // fabled do not have abilities in the botc online data
+  "spiritofivory": {
+    ability: "There can't be more than 1 extra evil player."
+  },
+  "doomsayer": {
+    ability: `If 4 or more players live, each living player may publicly
+choose (once per game) that a player of their own alignment dies.`,
+  },
+  "duchess": {
+    ability: `Each day, 3 players may choose to visit you.
+At night*, each visitor learns how many visitors are evil, but 1 gets false info.`,
+  },
+  "sentinel": {
+    ability: `There might be 1 extra or 1 fewer Outsider in play.`,
+  },
+  "stormcatcher": {
+    ability: `Name a good character. If in play, they can only
+die by execution, but evil players learn which player it is.`,
+  },
+}
+
+function getOverride(id: string): Override {
+  return overrideList[id] ?? {};
+}
+
+export const overrides = {
+  all: overrideList,
+  get: getOverride,
+  firstNight: (id: string): string | undefined => {
+    const override = getOverride(id);
+    return override.nights ?? override.firstNight ?? undefined;
+  },
+  otherNights: (id: string): string | undefined => {
+    const override = getOverride(id);
+    return override.nights ?? override.otherNights ?? undefined;
+  },
+  ability: (id: string): string | undefined => {
+    const override = getOverride(id);
+    return override.ability ?? undefined;
+  }
+};
--- src/js/botc/roles.ts
+++ src/js/botc/roles.ts
@@ -1,82 +1,28 @@
+// Script Tool roles
+//
+// Provides roleType but not abilities (these are in images which are assembled
+// to form the script PDF).
+//
+// Official characters from https://script.bloodontheclocktower.com/
 import script_roles from '../../../assets/data/roles.json';
-import botc_roles from '../../../assets/data/botc_online_roles.json';
+// Script Tool "night sheet"
+//
+// Gives a global ordering for all characters, for first night and other nights.
 import nightsheet from '../../../assets/data/nightsheet.json';
 
-interface Override {
-  ability?: string;
-  firstNight?: string | null;
-  otherNights?: string | null;
-}
+// clocktower.online (old online website) roles
+//
+// Unofficial, but quite up-to-date. Has abilities and storyteller instructions
+// ("reminders") for first and other nights. The reminder text is unofficial;
+// the wiki has detailed "how to run" instructions but not concise ones suitable
+// for a night sheet. There are also night orderings here, but they're ignored
+// in favor of the official script tool.
+import botc_roles from '../../../assets/data/botc_online_roles.json';
 
-const overrides: { [key: string]: Override } = {
-  "philosopher": {
-    firstNight: null,
-    otherNights: null,
-  },
-  "investigator": {
-    firstNight: null,
-  },
-  "chef": {
-    firstNight: "Show the number of pairs of neighboring evil players.",
-  },
-  "clockmaker": {
-    firstNight: "Show the number of places from Demon to closest Minion.",
-  },
-  "lunatic": {
-    firstNight: `If 7 or more players: <tab>Show THESE ARE YOUR MINIONS. Point to "Minions".
-    <tab>Show three bluffs.
-    <tab>Put the Lunatic to sleep. Wake the demon.
-    <tab>Show the YOU ARE token, and the Demon token.
-    <tab>Show THIS PLAYER IS and the Lunatic token, point to the Lunatic.
-    `
-  },
-  "cerenovus": {
-    firstNight: `The Cerenovus chooses a player and character. Wake the target.
-    Show THIS CHARACTER SELECTED YOU, the Cerenovus token, and the madness character.`,
-  },
-  "sailor": {
-    firstNight: null,
-  },
-  "poisoner": {
-    firstNight: "The Poisoner picks a player to poison.",
-  },
-  "fortuneteller": {
-    firstNight: "The Fortune Teller points to two players. Give a yes if one is the Demon (or red herring)."
-  },
-  "cultleader": {
-    firstNight: "The Cult Leader might change alignment to match an alive neighbor. If it changed, tell the Cult Leader their new alignment.",
-  },
-  "monk": {
-    otherNights: "The Monk protects a player from the Demon.",
-  },
-  "innkeeper": {
-    otherNights: "The Innkeeper points to two players, who are both safe from the Demon. One is drunk.",
-  },
-  "imp": {
-    otherNights: `The Imp kills a player. If they chose themselves,
-    replace an alive Minion with an Imp token. Show them YOU ARE and then the Imp token.`,
-  },
-
-  // fabled do not have abilities in the botc online data
-  "spiritofivory": {
-    ability: "There can't be more than 1 extra evil player."
-  },
-  "doomsayer": {
-    ability: `If 4 or more players live, each living player may publicly
-choose (once per game) that a player of their own alignment dies.`,
-  },
-  "duchess": {
-    ability: `Each day, 3 players may choose to visit you.
-At night*, each visitor learns how many visitors are evil, but 1 gets false info.`,
-  },
-  "sentinel": {
-    ability: `There might be 1 extra or 1 fewer Outsider in play.`,
-  },
-  "stormcatcher": {
-    ability: `Name a good character. If in play, they can only
-die by execution, but evil players learn which player it is.`,
-  },
-}
+// Custom overrides provided by this app. Most of these are simplifications to
+// the night sheet to surface more important instructions, but this also
+// includes fabled abilities which are otherwise not available anywhere.
+import { overrides } from './overrides';
 
 interface NightAction {
   details: string;
@@ -160,18 +106,13 @@ function createRoleData(): Map<string, CharacterInfo> {
   for (const role of botc_roles) {
     const id = role.id;
     const info = roles.get(id);
-    const override = overrides[id];
     if (info === undefined) {
       // a character in the online tool but not known to the script tool -
       // applies to Mephit (which was renamed)
       continue;
     }
 
-    if (override !== undefined && override.ability !== undefined) {
-      info.ability = override.ability;
-    } else {
-      info.ability = role.ability;
-    }
+    info.ability = overrides.ability(id) ?? role.ability;
 
     if (role.firstNightReminder != "") {
       const index = nightsheet.firstNight.indexOf(info.name);
@@ -179,12 +120,9 @@ function createRoleData(): Map<string, CharacterInfo> {
         console.warn(`${id} not found in night sheet`);
       }
       info.firstNight = {
-        details: role.firstNightReminder,
+        details: overrides.firstNight(id) ?? role.firstNightReminder,
         index,
       };
-      if (override !== undefined) {
-        info.firstNight.details = override.firstNight || "";
-      }
     }
     if (role.otherNightReminder != "") {
       const index = nightsheet.otherNight.indexOf(info.name);
@@ -192,21 +130,13 @@ function createRoleData(): Map<string, CharacterInfo> {
         console.warn(`${id} not found in night sheet`);
       }
       info.otherNights = {
-        details: role.otherNightReminder,
+        details: overrides.otherNights(id) ?? role.otherNightReminder,
         index,
-      }
-      if (override !== undefined) {
-        var details = override.otherNights;
-        // copy first night override of otherNights is omitted
-        if (details === undefined) {
-          details = override.firstNight;
-        }
-        info.otherNights.details = details || "";
       }
     }
   }
 
-  for (const id of Object.keys(overrides)) {
+  for (const id of Object.keys(overrides.all)) {
     if (botc_roles.find(c => c.id == id) === undefined) {
       // an override for a character not in Clocktower Online
       const info = roles.get(id);
@@ -214,10 +144,7 @@ function createRoleData(): Map<string, CharacterInfo> {
         console.error(`override info for unknown id ${id} `);
         continue;
       }
-      const override = overrides[id];
-      if (override.ability) {
-        info.ability = override.ability;
-      }
+      info.ability = overrides.get(id).ability ?? info.ability
     }
   }
 
