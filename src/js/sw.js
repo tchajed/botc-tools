52b05233cf0b6c854e7e36e2035654124606b0d8
Tej Chajed <tchajed@gmail.com>
2023-06-02 23:18:26 -0700

    Remove sw.js
    Seems to trigger a parcel bug (at least in dev builds).

--- src/js/sw.js
+++
@@ -1,5 +0,0 @@
-import { Workbox } from 'workbox-window';
-if (/* process.env.NODE_ENV == 'production' && */ 'serviceWorker' in navigator) {
-  const wb = new Workbox('./service-worker.js');
-  wb.register();
-}
