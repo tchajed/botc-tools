5309b544c6ba47690c424c97d70eec6d5a652876
Tej Chajed <tchajed@gmail.com>
2023-06-02 19:23:03 -0700

    Factor out header to make PWA setup easier

---
+++ src/js/sw.js
@@ -0,0 +1,5 @@
+import { Workbox } from 'workbox-window';
+if (process.env.NODE_ENV == 'production' && 'serviceWorker' in navigator) {
+  const wb = new Workbox('./service-worker.js');
+  wb.register();
+}
