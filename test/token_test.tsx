1fafcec5ce17181eea5984f8b666fd0b815cdf34
Tej Chajed <tchajed@gmail.com>
2023-06-05 13:24:23 -0700

    Refactor token grid test code

--- test/token_test.tsx
+++ test/token_test.tsx
@@ -1,18 +1,28 @@
 import React from "react";
-import { roles } from "../src/js/botc/roles";
+import { CharacterInfo, roles } from "../src/js/botc/roles";
 import { TokenSvg } from "../src/js/randomizer/tokens/token_svg";
+
+/** An SVG with all character tokens, rendered in a nice grid. */
+function TokenGrid(props: {
+  characters: CharacterInfo[],
+  numColumns: number
+}): JSX.Element {
+  const { characters, numColumns } = props;
+  const numRows = Math.ceil(characters.length / numColumns);
+  return <svg
+    width="100%"
+    viewBox={`0 0 ${240 * numColumns} ${240 * numRows}`}>
+    {characters.map((char, i) => {
+      const column = i % numColumns;
+      const row = Math.floor(i / numColumns);
+      return <TokenSvg character={char} x={column * 240} y={row * 240} key={char.id} />
+    })}
+  </svg>
+}
 
 export function AllTokens(): JSX.Element {
   const characters = [...roles.entries()].map(([_, character]) => character);
-  const numColumns = 6;
-  const numRows = Math.ceil(characters.length / numColumns);
   return <div className="main">
-    <svg width="100%" viewBox={`0 0 ${240 * numColumns} ${240 * numRows}`} scale="0.2">
-      {characters.map((char, i) => {
-        const column = i % numColumns;
-        const row = Math.floor(i / numColumns);
-        return <TokenSvg character={char} x={column * 240} y={row * 240} key={char.id} />
-      })}
-    </svg>
+    <TokenGrid characters={characters} numColumns={6} />
   </div>
 }
