707b72b9a80f197a24c952e1501846f913889fb5
Tej Chajed <tchajed@gmail.com>
2023-06-10 11:52:35 -0700

    Use a library for search matching

--- src/js/listing/search.ts
+++ src/js/listing/search.ts
@@ -1,4 +1,5 @@
 import { ScriptData } from "../botc/script";
+import { matchSorter } from "match-sorter";
 
 const FAVORITES: number[] = (() => {
   // 360 Visitors
@@ -9,41 +10,6 @@ const FAVORITES: number[] = (() => {
   return favorites.split(",").map((s) => parseInt(s));
 })();
 
-function normalize(title: string): string {
-  return title.toLowerCase().replaceAll(/ +/g, " ").replaceAll(/[']/g, "");
-}
-
-type Tuple = { n1: number; n2: string };
-
-function lexCompare(x: Tuple, y: Tuple): number {
-  if (x.n1 != y.n1) {
-    return x.n1 - y.n1;
-  }
-  return x.n2.localeCompare(y.n2);
-}
-
-function matchPriority(s: ScriptData, q: string): number {
-  if (s.title.startsWith(q)) {
-    return -100;
-  }
-  const title = normalize(s.title);
-  const normq = normalize(q);
-  if (title.startsWith(normq)) {
-    return -99;
-  }
-  if (title.split(" ").some((w) => w.startsWith(normq))) {
-    return -97;
-  }
-  if (title.includes(normq)) {
-    return -98;
-  }
-  return 0;
-}
-
-function measure(s: ScriptData, q: string): Tuple {
-  return { n1: matchPriority(s, q), n2: s.title };
-}
-
 export function queryMatches(
   scripts: ScriptData[],
   query: string
@@ -52,19 +18,11 @@ export function queryMatches(
   if (query == "") {
     matches = scripts.filter((s) => FAVORITES.includes(s.pk));
   } else {
-    const q = normalize(query);
-    if (q == "") {
-      matches = scripts;
-    } else {
-      matches = scripts.filter((s) => normalize(s.title).includes(q));
-    }
+    matches = matchSorter(scripts, query, { keys: ["title"] });
   }
-  matches.sort((s1, s2) => {
-    return lexCompare(measure(s1, query), measure(s2, query));
-  });
   return matches;
 }
 
-export function searchNormalize(s: string): string {
-  return normalize(s);
+export function searchNormalize(query: string): string {
+  return query.toLowerCase().replaceAll(/ +/g, " ").replaceAll(/[']/g, "");
 }
