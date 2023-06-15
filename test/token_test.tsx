efddfb0a9777dd7cb5e4b02d4367e97a45c948cb
Tej Chajed <tchajed@gmail.com>
2023-06-15 14:57:07 -0700

    Improve token test page

--- test/token_test.tsx
+++ test/token_test.tsx
@@ -1,27 +1,15 @@
-import { CharacterInfo, roles } from "../src/js/botc/roles";
+import { roles } from "../src/js/botc/roles";
 import { TokenCanvas } from "../src/js/randomizer/tokens/token_canvas";
 import React from "react";
 
-/** An SVG with all character tokens, rendered in a nice grid. */
-function TokenGrid(props: {
-  characters: CharacterInfo[];
-  numColumns: number;
-}): JSX.Element {
-  const { characters, numColumns: _ } = props;
-  return (
-    <div>
-      {characters.map((char) => {
-        return <TokenCanvas character={char} key={char.id} size={100} />;
-      })}
-    </div>
-  );
-}
-
+/** All the tokens */
 export function AllTokens(): JSX.Element {
   const characters = [...roles.entries()].map(([_, character]) => character);
   return (
     <div className="main">
-      <TokenGrid characters={characters} numColumns={6} />
+      {characters.map((char) => {
+        return <TokenCanvas character={char} key={char.id} size={120} />;
+      })}
     </div>
   );
 }
