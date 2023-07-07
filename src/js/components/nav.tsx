a2ad6316f2c9f18a38e1e66cccd2525878021f83
Tej Chajed <tchajed@gmail.com>
2023-07-06 21:30:18 -0700

    Fix current tab highlight

--- src/js/components/nav.tsx
+++ src/js/components/nav.tsx
@@ -17,7 +17,7 @@ const NavItem = styled.a`
   &:visited {
     color: white;
     &.current {
-      border-bottom: "0.2rem solid ${(props) => props.theme.color.secondary}";
+      border-bottom: 0.2rem solid ${(props) => props.theme.color.secondary};
       // box-shadow: 0 0 10px 5px rgb(255, 235, 122);
     }
   }
