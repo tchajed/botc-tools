3463b6e4733c74767306d9025812f04fa6a0363c
Tej Chajed <tchajed@gmail.com>
2023-07-06 21:26:16 -0700

    Fix token test page

--- test/token_test.tsx
+++ test/token_test.tsx
@@ -1,12 +1,24 @@
 import { roles } from "../src/js/botc/roles";
 import { TokenCanvas } from "../src/js/randomizer/tokens/token_canvas";
-import React from "react";
+import { Global, css } from "@emotion/react";
+import { GlobalStyle } from "styles/global_style";
 
 /** All the tokens */
 export function AllTokens(): JSX.Element {
   const characters = [...roles.entries()].map(([_, character]) => character);
   return (
     <div className="main">
+      <Global
+        styles={[
+          GlobalStyle,
+          css`
+            .main {
+              max-width: calc(5 * 120px);
+              margin: auto;
+            }
+          `,
+        ]}
+      />
       {characters.map((char) => {
         return <TokenCanvas character={char} key={char.id} size={"120px"} />;
       })}
